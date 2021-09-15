5a69b36cd58868f46ab2af999111688c21b1b5d6
Ahmed Mohamed 3adl <80198727+Ahmed101Mohammed@users.noreply.github.com>
2021-09-15 12:37:41 +0200

    Remove not useful comments

--- your_pay.py
+++ your_pay.py
@@ -1,23 +1,3 @@
-# try:
-#     score = float(input('Enter Score: '))
-# except:
-#     print('Error: Enter number plase')
-#     quit()
-
-# if score <= 1 and score >= 0:
-#     if score >= 0.9:
-#         grade = 'A'
-#     elif score >= 0.8:
-#         grade = 'B'
-#     elif score >= 0.7:
-#         grade = 'C'
-#     elif score >= 0.6:
-#         grade = 'D'
-#     else:
-#         grade = 'F'
-# else: grade = 'Error, plase enter number between 0 and 1'
-
-# print(grade)
 try:
     hrs = float(input('Enter Hours: '))
     rate = float(input('Enter rate: '))
